8265033f5acee9a34c6510ccf0f47734bf86bf15
Filip Krestan <krestfi1@fit.cvut.cz>
2018-07-05 12:40:55 +0200

    Add basic tests for merge operation

--- misc/test/basic.c
+++ misc/test/basic.c
@@ -33,6 +33,7 @@
 int main(int argc, char **argv)
 {
   struct bloom bloom;
+  struct bloom bloom2;
 
   printf("----- Basic tests with static library -----\n");
   assert(bloom_init(&bloom, 0, 1.0) == 1);
@@ -54,5 +55,44 @@ int main(int argc, char **argv)
   assert(bloom_add(&bloom, "hello", 5) > 0);
   assert(bloom_check(&bloom, "hello", 5) == 1);
   bloom_free(&bloom);
+
+  printf("----- Basic tests with static library - merge -----\n");
+  assert(bloom_init(&bloom, 1002, 0.1) == 0);
+  assert(bloom.ready == 1);
+
+  assert(bloom2.ready != 1);
+  assert(bloom_merge(&bloom, &bloom2) == -1);
+
+  assert(bloom_init(&bloom2, 1003, 0.1) == 0);
+  assert(bloom2.ready == 1);
+  assert(bloom_merge(&bloom, &bloom2) == -2);
+  bloom_free(&bloom2);
+
+  assert(bloom_init(&bloom2, 1002, 0.2) == 0);
+  assert(bloom2.ready == 1);
+  assert(bloom_merge(&bloom, &bloom2) == -3);
+  bloom_free(&bloom2);
+
+  assert(bloom_init(&bloom2, 1002, 0.1) == 0);
+  assert(bloom2.ready == 1);
+  assert(bloom_add(&bloom, "hello world", 11) == 0);
+  assert(bloom_add(&bloom, "hello", 5) == 0);
+  assert(bloom_add(&bloom2, "hello world 2", 13) == 0);
+  assert(bloom_add(&bloom2, "hello 2", 7) == 0);
+  assert(bloom_merge(&bloom, &bloom2) == 0);
+  assert(bloom_check(&bloom2, "hello world", 11) == 0);
+  assert(bloom_check(&bloom2, "hello", 5) == 0);
+  assert(bloom_check(&bloom2, "hello world 2", 13) == 1);
+  assert(bloom_check(&bloom2, "hello 2", 7) == 1);
+  assert(bloom_check(&bloom, "hello world", 11) == 1);
+  assert(bloom_check(&bloom, "hello", 5) == 1);
+  assert(bloom_check(&bloom, "hello world 2", 13) == 1);
+  assert(bloom_check(&bloom, "hello 2", 7) == 1);
+  bloom_print(&bloom);
+  bloom_print(&bloom2);
+
+  bloom_free(&bloom);
+  bloom_free(&bloom2);
+
   printf("----- DONE Basic tests with static library -----\n");
 }
